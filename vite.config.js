1bb0badd3dd7e8b8e511fb54253e515e0760aa8b
Harpreet Kour Arora <harpreetkourarora1@gmail.com>
2026-03-01 16:37:51 +0530

    Add vite.config.js to support multi-page build and fix 404 errors

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                blogs: resolve(__dirname, 'blogs.html'),
+                'blog-post': resolve(__dirname, 'blog-post.html'),
+            },
+        },
+    },
+});
